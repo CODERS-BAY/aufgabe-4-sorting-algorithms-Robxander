0b93610ed9d1f21f9e499dbaffa260b23a282acc
Robert <r.kohl@xentego.at>
2023-11-07 14:39:03 +0100

    Array method created. Print method created. About doing measure method.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,30 @@
+import sort.Sorting;
+
+
+
 public class Main {
 
     public static void main(String[] args) {
 
         // TODO Initialise an int array with random numbers
+        int[] randomArray = Sorting.getRandomArray(7);
 
 
         // TODO Sort the array with your sorting algorithm
-
+//        sortArray(randomArray);
 
         // TODO Print the array to the console
+        Sorting.printArray(randomArray);
 
         // TODO Do a runtime analysis of your algorithm
+
+//        System.out.println(currentTimeMillis());
+//        Sorting.measureRuntime();
         //  For which array size does the execution time start to rise significantly?
 
 
         // TODO Compare the execution time of your algorithm with the execution time of your QuickSort or BubbleSort implementation
-        
+
 
         // TODO Print the iterations counters on the console
     }
--- src/main/java/sort/Sorting.java
+++ src/main/java/sort/Sorting.java
@@ -1,17 +1,44 @@
 package sort;
+import static java.lang.System.currentTimeMillis;
 
 public class Sorting {
 
     // TODO Write a method to generate an array of size N initialised with random numbers
 
+    public static int[] getRandomArray(int size) {
+        int[] array = new int[size];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (10 * Math.random());
+        }
+        return array;
+    }
 
     // TODO Implement a sorting algorithm of your choice
+public static void sortArray(int[] array){
+    System.out.println("Hier ist das zu sortierende Array: ");
+    printArray(array);
 
+}
 
     // TODO Implement a printArray method
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+        }
+        System.out.println();
+    }
 
 
     // TODO Write a method to do a runtime analysis of your algorithm.
+    public static long measureRuntime (int[] array){
+        long timeStart = currentTimeMillis();
+         sortArray(array);
+        long timeEnd = currentTimeMillis();
+        return (timeEnd -timeStart);
+    }
+
+
+
     //  For which array size does the execution time start to rise significantly?
 
 
