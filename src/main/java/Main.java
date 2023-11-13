dacccabdb824d69d576cf544cdead26f9a04c420
Robert <r.kohl@xentego.at>
2023-11-13 19:04:45 +0100

    corrected QuickSort and counter implemented

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,28 +6,35 @@ public class Main {
     public static void main(String[] args) {
 
         //Initialise an int array with random numbers
-        int arraySize = 100;
+        int arraySize = 1000;
         int[] randomArray = Sorting.getRandomArray(arraySize);
+        int begin = 0;
+        int end = arraySize -1;
+
         Sorting.printArray(randomArray);
 
 
         //Sort the array with your sorting algorithm
-        Sorting.bubbleSort(randomArray);
+       // Sorting.bubbleSort(randomArray);
 
         //Print the array to the console
         Sorting.printArray(randomArray);
 
         //Do a runtime analysis of your algorithm
         randomArray = Sorting.getRandomArray(arraySize);
+
         System.out.println(Sorting.measureRuntimeBubblesort(randomArray));
-        System.out.println(Sorting.measureRuntimeQuicksort(randomArray));
+        System.out.println(Sorting.measureRuntimeQuicksort(randomArray, begin , end));
 
         //  For which array size does the execution time start to rise significantly?
-
+        // From a number of 10.000 array size it start to rise significant.
 
         // TODO Compare the execution time of your algorithm with the execution time of your QuickSort or BubbleSort implementation
 
 
-        // TODO Print the iterations counters on the console
+        // Print the iterations counters on the console
+        // In order to count iteration bubbleCount turn off task 2
+        System.out.println("Get Bubble Counts: " + Sorting.getBubbleCounts());
+        System.out.println("Get Quick Counts: " + Sorting.getQuickCounts());
     }
 }
--- src/main/java/sort/Sorting.java
+++ src/main/java/sort/Sorting.java
@@ -3,8 +3,10 @@ package sort;
 import static java.lang.System.nanoTime;
 
 public class Sorting {
+    private static int bubbleCounter = 0;
+    private static int quickCounter = 0;
 
-    // TODO Write a method to generate an array of size N initialised with random numbers
+    // Write a method to generate an array of size N initialised with random numbers
 
     public static int[] getRandomArray(int size) {
         int[] array = new int[size];
@@ -14,14 +16,15 @@ public class Sorting {
         return array;
     }
 
-    // TODO Implement a sorting algorithm of your choice
-//public static void sortArray(int[] array){
+    // Implement a sorting algorithm of your choice
+    // I tried to make HeapSort, but it's too complicated, thus I use bubbleSort
+
+// public static void sortArray(int[] array){
 //    System.out.println("Hier ist das zu sortierende Array: ");
 //    printArray(array);
 //}
 
     public static void bubbleSort(int[] array) {
-        // TODO Implement algorithm
         for (int i = 0; i < array.length; i++) {
             boolean isSorted = true;
             for (int j = 0; j < array.length - 1; j++) {
@@ -34,6 +37,7 @@ public class Sorting {
                 }
 //                printArray(array);
             }
+            bubbleCounter++;
 //            System.out.println();
             if (isSorted) {
                 return;
@@ -41,25 +45,43 @@ public class Sorting {
         }
     }
 
-    public static void quickSort(int[] array) {
-        int pivot = array[array.length - 1];
-        int countL = 0;
-        int countR = 0;
-        for (int i = 0; i < array.length; i++) {
-            countL = i;
-            if (array[i] > pivot) {
-                break;
-            }
-        }
-        for (int i = array.length-2; i > -1; i--) {
-            countR = i;
-            if (array[i] < pivot) {
-                break;
-            }
-        }
+
+    public static int getBubbleCounts(){
+        return bubbleCounter;
     }
 
-    // TODO Implement a printArray method
+
+    public static void quickSort(int[] numbers, int begin, int end) {
+
+        if (begin < end) {
+            int partitionIndex = partition(numbers, begin, end);
+            quickSort(numbers, begin, partitionIndex - 1);
+            quickSort(numbers, partitionIndex + 1, end);
+        }
+    }
+    private static int partition(int[] numbers, int begin, int end) {
+        int pivot = numbers[end]; int i = (begin - 1);
+        for (int j = begin; j < end; j++) {
+            if (numbers[j] <= pivot) {
+                i++;
+                int swapTemp = numbers[i];
+                numbers[i] = numbers[j];
+                numbers[j] = swapTemp;
+            }
+        }
+        quickCounter++;
+        int swapTemp = numbers[i + 1];
+        numbers[i + 1] = numbers[end];
+        numbers[end] = swapTemp;
+        return i + 1;
+    }
+
+    public static int getQuickCounts (){
+        return quickCounter;
+    }
+
+
+    // Implement a printArray method
     public static void printArray(int[] array) {
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i]);
@@ -68,32 +90,31 @@ public class Sorting {
     }
 
 
-    // TODO Write a method to do a runtime analysis of your algorithm.
-    public static long measureRuntimeBubblesort (int[] array){
+    // Write a method to do a runtime analysis of your algorithm.
+    public static long measureRuntimeBubblesort(int[] array) {
         long timeStart = nanoTime();
-         bubbleSort(array);
+        bubbleSort(array);
         long timeEnd = nanoTime();
 
-        long difTime = (timeEnd -timeStart);
-        return reSize(difTime);
-    }
-
-    public static long measureRuntimeQuicksort (int[] array){
-        long timeStart = nanoTime();
-        quickSort(array);
-        long timeEnd = nanoTime();
-
-        long difTime = (timeEnd -timeStart);
+        long difTime = (timeEnd - timeStart);
         return reSize(difTime);
     }
 
 
+    public static long measureRuntimeQuicksort(int[] array, int begin, int end) {
+        long timeStart = nanoTime();
+        quickSort(array, begin, end);
+        long timeEnd = nanoTime();
 
+        long difTime = (timeEnd - timeStart);
+        return reSize(difTime);
+    }
 
-public static long reSize(long difTime){
-        if(difTime > 1e6){
+    // Make to Millisecond, Mikrosecond and Nanosecond
+    public static long reSize(long difTime) {
+        if (difTime > 1e6) {
             System.out.print("Millisekunden: ");
-        return (long)(difTime/1e6);
+            return (long) (difTime / 1e6);
 //        } else if (difTime > 1000){
 //            System.out.print("Mikrosekunden: ");
 //            return difTime /1000;
@@ -101,11 +122,11 @@ public static long reSize(long difTime){
             System.out.print("Nanosekunden: ");
             return difTime;
         }
-}
+    }
 
 
     //  For which array size does the execution time start to rise significantly?
-
+    // It start rising with BubbleSort at a arraysize of 100.000, because complexity is n^2
 
     // TODO Add global static counter variables to count the number of iterations for each algorithm
 
